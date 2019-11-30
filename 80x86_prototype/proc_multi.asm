ae326c5093dcdaf1d83eb0a4bed745e63d048868
CSGlobalOffensive <zhoubw1999@126.com>
2019-11-30 15:37:51 +0800

    add proc_multi

--- 80x86_prototype/proc_multi.asm
+++ 80x86_prototype/proc_multi.asm
@@ -3,5 +3,24 @@
 ; 过程 AL <- AL*AH
 ; 输出 AL
 MULTI PROC NEAR
-
+    PUSH BX
+    MOV BH, 7 ;循环次数
+    MOV BL, 0
+PANDUAN:
+    TEST AH, 80H ;乘数当前位是否为1
+    JNZ LEIJIA
+YIWEI:
+    SHL AH, 1
+    SHL BL, 1
+    JMP XUNHUAN
+LEIJIA:
+    ADD BL, AL ;加上被乘数
+    JMP YIWEI
+XUNHUAN:
+    SUB BH, 1
+    JNZ PANDUAN
+    MOV AL, BL ;AL存放结果
+    MOV AH, BH ;AH结果一定为零
+    POP BX
+    RET
 MULTI ENDP
