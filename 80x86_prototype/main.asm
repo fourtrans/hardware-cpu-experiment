3a22b3bdee6ca18f7b8ba30a86e1c6c325f7f5d3
oowsxq <oowsxq@users.noreply.github.com>
2019-11-30 16:45:49 +0800

    stage1 done

--- 80x86_prototype/main.asm
+++ 80x86_prototype/main.asm
@@ -51,8 +51,22 @@ LCG ENDP
 ; 过程 AL <- AL*AH
 ; 输出 AL
 MULTI PROC NEAR
-    mul ah
-    ret
+    PUSH BX
+    MOV BH, 8 ;循环次数
+    MOV BL, 0
+    PANDUAN:
+    ROL AH, 1
+    SHL BL, 1
+    TEST AH, 1 ;乘数当前位是否为1
+    JZ XUNHUAN
+    ADD BL, AL ;加上被乘数
+    XUNHUAN:
+    SUB BH, 1
+    JNZ PANDUAN
+    MOV AL, BL ;AL存放结果
+    MOV AH, BH ;AH结果一定为零
+    POP BX
+    RET
 MULTI ENDP
 
 ; 用于输出AL中的内容
