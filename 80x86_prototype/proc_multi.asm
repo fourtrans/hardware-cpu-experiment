d28862a709824a4dd62a89cd6b08de5edce14ab9
CSGlobalOffensive <zhoubw1999@126.com>
2019-12-07 10:25:45 +0800

    usable multi proc with 4 regs

--- 80x86_prototype/proc_multi.asm
+++ 80x86_prototype/proc_multi.asm
@@ -3,20 +3,21 @@
 ; 过程 AL <- AL*AH
 ; 输出 AL
 MULTI PROC NEAR
-    PUSH BX
-    MOV BH, 8 ;循环次数
-    MOV BL, 0
+    PUSH BX ;保护BH和BL
+    MOV BH, 1 ;循环次数，通过循环右移实现
+    MOV BL, 0 ;结果高位
 PANDUAN:
-    ROL AH, 1
-    SHL BL, 1
     TEST AH, 1 ;乘数当前位是否为1
-    JZ XUNHUAN
+    JLE XUNHUAN
     ADD BL, AL ;加上被乘数
 XUNHUAN:
-    SUB BH, 1
-    JNZ PANDUAN
-    MOV AL, BL ;AL存放结果
-    MOV AH, BH ;AH结果一定为零
+    ROR BH, 1 ;循环右移
+    RCR BL, 1 ;带进位的循环右移
+    RCR AH, 1 ;带进位的循环右移
+    TEST BH, 1 ;循环是否终止
+    JLE PANDUAN
+    MOV AL, AH
+    MOV AH, BL
     POP BX
     RET
 MULTI ENDP
